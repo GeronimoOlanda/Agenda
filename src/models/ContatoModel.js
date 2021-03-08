4a9c8c6fb3d9cae75f8b8bd721bb565210a08aaf
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-03-08 04:58:24 -0300

    Listando dados do banco de dados MongoDB

--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -35,11 +35,7 @@ function Contato(body) {
   this.contato = null;
 }
 
-Contato.buscaPorId = async function (id) {
-  if (typeof id !== 'string') return;
-  const user = await ContatoModel.findById(id);
-  return user;
-};
+
 
 Contato.prototype.register = async function () {
   this.valida();
@@ -81,5 +77,16 @@ Contato.prototype.edit = async function(id) {
   this.valida();
   if(this.errors.length > 0) return;
   this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true });
+};
+//métodos estaticos, sao os que  nao vao para o prototype
+Contato.buscaPorId = async function (id) {
+  if (typeof id !== 'string') return;
+  const contato = await ContatoModel.findById(id);
+  return contato;
+};
+
+Contato.buscaContatos = async function () {
+  const contatos = await ContatoModel.find().sort({criadoEm: -1})
+  return contatos;
 };
 module.exports = Contato;
--- src/models/homeModel.js
+++ src/models/homeModel.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
 
-//trabalhando com esquemas(regras de negocios)
-//estamos fazendo do jeito que queremos que esteja na base de dados
 const HomeSchema = new mongoose.Schema({
-    titulo: {
-        type: String,
-        required: true
-    },
-    descricao: String,
-
+  titulo: { type: String, required: true },
+  descricao: String
 });
 
 const HomeModel = mongoose.model('Home', HomeSchema);
